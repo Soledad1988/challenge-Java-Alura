08397fd73cd20e87cdaf56e5f437d2ca168166ca
Brenda <brentorres26@gmail.com>
2023-03-14 13:53:23 -0300

    Manejo de excepciones

--- challenge_Java_Alura/src/challenge_Java_Alura/Conversor.java
+++ challenge_Java_Alura/src/challenge_Java_Alura/Conversor.java
@@ -2,6 +2,7 @@ package challenge_Java_Alura;
 
 import javax.swing.JOptionPane;
 
+
 public class Conversor {
 
 	public static void main(String[] args) {
@@ -13,10 +14,35 @@ public class Conversor {
 			Object[] monedas = {"De Peso a Dolar", "De Peso a Euro", "De Peso a Libras",
 					"De peso a Yen", "De Peso a Won Coreano", "De Dolar a Peso", "De Euro a Peso", "De Libras a Peso"};
 			
-			JOptionPane.showInputDialog(null, "Elije la moneda a la que deseas convertir tu dinero", null, JOptionPane.QUESTION_MESSAGE, 
-					null, monedas, monedas[0]);
+			Object elegirMoneda = JOptionPane.showInputDialog(null,"Elije la moneda a la que deseas convertir tu dinero", 
+					"Menu",JOptionPane.QUESTION_MESSAGE,null,monedas, monedas[0]);
+			
+			// por si no elige ninguna opcion de conversion
+			if (elegirMoneda==null) {
+				JOptionPane.showInputDialog(null,"Salio del conversor");
+				
+			// por si escoge no seguir se sale del programa
+				System.exit(0);
+			}
+			
+			try {
+				//ingresar valor a convertir
+				String cantidadDinero=JOptionPane.showInputDialog("Ingrese la cantidad de dinero que desea convertir ");
+				
+			
+			}
+			// por si se  ingresa un valor  no permitido
+			catch(NumberFormatException | NullPointerException ex) {
+				JOptionPane.showMessageDialog(null, "Solo se permiten números");
+				
+			}
+			
+			
 		}else {
 			JOptionPane.showMessageDialog(null,"Por el momento no esta habilitado");
 		}
 	}
+	
+	
+	
 }
