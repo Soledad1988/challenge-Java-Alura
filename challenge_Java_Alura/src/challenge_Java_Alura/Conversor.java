6f5e7ca86b83082aaeddac354e630d9c8c5bdf65
Brenda <brentorres26@gmail.com>
2023-03-15 22:16:28 -0300

    Pruba conversor y ajuste a mensajes

--- challenge_Java_Alura/src/challenge_Java_Alura/Conversor.java
+++ challenge_Java_Alura/src/challenge_Java_Alura/Conversor.java
@@ -11,30 +11,37 @@ public class Conversor {
 				null, new Object[] {"Conversor de monedas","Conversor de Temperatura"},"Seleccionar")).toString();
 		
 		if(menu.toString().equals("Conversor de monedas")) {
-			Object[] monedas = {"De Peso a Dolar", "De Peso a Euro", "De Peso a Libras",
-					"De peso a Yen", "De Peso a Won Coreano", "De Dolar a Peso", "De Euro a Peso", "De Libras a Peso"};
 			
-			Object elegirMoneda = JOptionPane.showInputDialog(null,"Elije la moneda a la que deseas convertir tu dinero", 
-					"Menu",JOptionPane.QUESTION_MESSAGE,null,monedas, monedas[0]);
+			String moneda = (JOptionPane.showInputDialog(null,"Selecciones la moneda","Monedas",JOptionPane.PLAIN_MESSAGE,
+					null, new Object[] {"De Peso a Dolar", "De Peso a Euro", "De Peso a Libras",
+							"De peso a Yen", "De Peso a Won Coreano", "De Dolar a Peso",
+							"De Euro a Peso","De Libras a Peso"},"Seleccionar")).toString();
 			
 			// por si no elige ninguna opcion de conversion
-			if (elegirMoneda==null) {
+			if (moneda==null) {
 				JOptionPane.showInputDialog(null,"Salio del conversor");
 				
 			// por si escoge no seguir se sale del programa
 				System.exit(0);
 			}
 			
-			try {
-				//ingresar valor a convertir
-				String cantidadDinero=JOptionPane.showInputDialog("Ingrese la cantidad de dinero que desea convertir ");
-				
+			switch(moneda) {
+			case "De Peso a Dolar":
+				convertir(315.20,"de peso a dolar");
+			break;
 			
-			}
-			// por si se  ingresa un valor  no permitido
-			catch(NumberFormatException | NullPointerException ex) {
-				JOptionPane.showMessageDialog(null, "Solo se permiten números");
-				
+			case "De Peso a Euro":
+				convertir(250.20,"de peso a Euro");
+			break;
+			
+			case "De Peso a Libras":
+				JOptionPane.showMessageDialog(null,"De Peso a Libras");
+			break;
+			
+			
+			default:
+				JOptionPane.showMessageDialog(null,"De Peso a Libras");
+			break;
 			}
 			
 			
@@ -43,6 +50,23 @@ public class Conversor {
 		}
 	}
 	
+	//conversor
 	
-	
+	static void convertir(double valorMoneda, String moneda) {
+		double valorIngresado = 0;
+		
+		try {
+			valorIngresado = Double.parseDouble(JOptionPane.showInputDialog("ingrese la cantidad de dinero que desea convertir "));
+		}catch(NumberFormatException ex){
+			JOptionPane.showMessageDialog(null, "Solo se permiten números");
+		}
+		
+		double conversion = valorIngresado / valorMoneda;
+		
+		conversion = (double)Math.round(conversion * 100d)/100;
+		
+		JOptionPane.showInternalMessageDialog(null, "Representa "+conversion +" Dolares");
+	}
+
+		
 }
